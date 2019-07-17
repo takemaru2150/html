750ff1e6aab533f057bdcee99a655c6b5d8fc611
sakurai <takeshi2jp1000@gmail.com>
2019-07-17 11:38:23 +0900

    gnav_spの作成前

--- toppage/src/lib.js
+++ toppage/src/lib.js
@@ -2,6 +2,6 @@ import $ from 'jquery';
 $(function(){
     $('.gnav_trigger').on('click', function() {
         $(this).toggleClass('_active');
-        $('.gnav').toggleClass('_active');
+        $('.gnav_sp').toggleClass('_active');
     })
 })
